36eef79c3c8f6c2482e144ec8d70aba55b76c425
Filipe Ferreira <filipe@graphik.io>
2017-09-08 15:44:19 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,76 @@
+komputer maschine
+======
+
+New Machine is a shell script to setup a new macOS installation with all my favourite design, development and entertainment software.
+
+
+## The setup
+
+**macOS tools:**
+* [Homebrew](http://brew.sh/) for managing operating system libraries.
+
+**Unix related:**
+* [fish](https://fishshell.com/) as your shell.
+* [git](https://git-scm.com/) for version control.
+* [gnupg2](https://fishshell.com/) for encryption and signing.
+* [OpenSSL](https://www.openssl.org/) for Transport Layer Security (TLS).
+* [tmux](http://tmux.github.io/) for saving project state and switching between projects.
+* [Vim](http://www.zsh.org/) for a powerful text editor.
+* [zsh](http://www.zsh.org/) as a second shell.
+
+**PGP & Yubikey:**
+* [gnupg21](https://gnupg.org/) 
+* [pinentry-mac](https://github.com/GPGTools/pinentry-mac) 
+
+**Image tools:**
+* [ImageMagick](https://www.imagemagick.org/) for converting, cropping, resizing and renaming images in the command line interface.
+
+**Programming languages and configuration:**
+* [Node.js](https://nodejs.org/) for running JavaScript applications.
+* [npm](https://www.npmjs.com/) for installing JavaScript packages.
+* [yarn](https://yarnpkg.com/) for installing JavaScript packages.
+
+**Applications for design, productivity and entertainment:**
+* [Alfred](https://www.alfredapp.com/) for increased productivity and efficiency with macOS.
+* [AppCleaner](https://freemacsoft.net/appcleaner/) to help fully remove applications.
+* [Brave](https://brave.com/) for browsing the web fast and secure.
+* [Caffeine](http://lightheadsw.com/caffeine/) to keep your Mac awake.
+* [Google Chrome](https://www.google.com/chrome/) for browsing the web.
+* [CyberDuck](https://cyberduck.io/) for FTP transfers.
+* [DropBox](https://www.dropbox.com/) for cloud storage.
+* [Evernote](https://evernote.com/) to get thoughts organized.
+* [FontExplorer X](https://www.fontexplorerx.com/) for fonts organization.
+* [FontPrep](https://www.fontexplorerx.com/) for font generation.
+* [Firefox](https://www.mozilla.org/en-US/firefox/new/) for browsing the web.
+* [iTerm2](https://www.iterm2.com/) for a nicer terminal.
+* [KeePassX](https://www.keepassx.org/) for password managment.
+* [Mullvad](https://mullvad.net/) for online privacy.
+* [Slack](https://slack.com/) for team communication.
+* [Spotify](https://www.spotify.com/) for music.
+* [Sketch](https://www.sketchapp.com/) for digital design.
+* [Sketch Toolbox](http://sketchtoolbox.com/) to manage plugins in Sketch.
+* [Sublime Text 3](https://www.sublimetext.com/3) as a text editor.
+* [The Unarchiver](https://theunarchiver.com/) for extracting archives that are not supported by macOS out of the box.
+* [Thunderbird](https://www.mozilla.org/) as an email client.
+* [Tor](https://www.torproject.org/) for online privacy.
+* [Tor Browser](https://www.torproject.org/projects/torbrowser.html.en) for browsing the web privately.
+* [Transmission](https://transmissionbt.com/) for torrents.
+* [Vagrant](https://www.vagrantup.com/) for creating and configuring development environments.
+* [Vagrant Manager](http://vagrantmanager.com/) to keep all vagrant machines in one place.
+* [VeraCrypt](https://veracrypt.codeplex.com/) for disk encryption.
+* [VirtualBox](https://www.virtualbox.org/) for x86 hardware virtualization.
+* [VLC](http://vagrantmanager.com/) to watch all kind of video files.
+
+**Applications for security:**
+* [BlockBlock](https://objective-see.com/products/blockblock.html) for monitoring persistence locations.
+* [DHS](https://objective-see.com/products/dhs.html) to scan for dylib hijacking.
+* [KnockKnock](https://objective-see.com/products/knockknock.html) to uncover persistently installed software.
+* [OverSight](https://objective-see.com/products/oversight.html) for monitoring mic and webcam.
+* [Security Growler](https://pirate.github.io/security-growler/) for notification whenever SSH, VNC, sudo, or other auth events occur.
+* [TaskExplorer](https://objective-see.com/products/taskexplorer.html) to visually explore all running tasks.
+
+## License
+Feel free to use it.
+
+## Credits
+Inspired by thoughtbot's [laptop](https://github.com/thoughtbot/laptop/).
---
+++ newMachine.sh
@@ -0,0 +1,174 @@
+#!/bin/sh
+# New Machine Setup 
+
+machine_echo() {
+  local fmt="$1"; shift
+  printf "\n$fmt\n" "$@"
+}
+
+append_to_zshrc() {
+  local text="$1" zshrc
+  local skip_new_line="${2:-0}"
+
+  if [ -w "$HOME/.zshrc.local" ]; then
+    zshrc="$HOME/.zshrc.local"
+  else
+    zshrc="$HOME/.zshrc"
+  fi
+
+  if ! grep -Fqs "$text" "$zshrc"; then
+    if [ "$skip_new_line" -eq 1 ]; then
+      printf "%s\n" "$text" >> "$zshrc"
+    else
+      printf "\n%s\n" "$text" >> "$zshrc"
+    fi
+  fi
+}
+
+# warns if the script fails
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+
+if [ ! -d "$HOME/.bin/" ]; then
+  mkdir "$HOME/.bin"
+fi
+
+if [ ! -f "$HOME/.zshrc" ]; then
+  touch "$HOME/.zshrc"
+fi
+
+# shellcheck disable=SC2016
+append_to_zshrc 'export PATH="$HOME/.bin:$PATH"'
+
+#
+sudo chown -R $(whoami) /usr/local
+
+# 
+HOMEBREW_PREFIX="/usr/local"
+
+if [ -d "$HOMEBREW_PREFIX" ]; then
+  if ! [ -r "$HOMEBREW_PREFIX" ]; then
+    sudo chown -R "$LOGNAME:admin" /usr/local
+  fi
+else
+  sudo mkdir "$HOMEBREW_PREFIX"
+  sudo chflags norestricted "$HOMEBREW_PREFIX"
+  sudo chown -R "$LOGNAME:admin" "$HOMEBREW_PREFIX"
+fi
+
+# Update shell to zsh
+update_shell() {
+  local shell_path;
+  shell_path="$(which zsh)"
+
+  machine_echo "Changing your shell to zsh ..."
+  if ! grep "$shell_path" /etc/shells > /dev/null 2>&1 ; then
+    machine_echo "Adding '$shell_path' to /etc/shells"
+    sudo sh -c "echo $shell_path >> /etc/shells"
+  fi
+  chsh -s "$shell_path"
+}
+
+case "$SHELL" in
+  */zsh)
+    if [ "$(which zsh)" != '/bin/zsh' ] ; then
+      update_shell
+    fi
+    ;;
+  *)
+    update_shell
+    ;;
+esac
+
+# Install Homebrew
+if ! command -v brew >/dev/null; then
+  machine_echo "Installing Homebrew ..."
+    curl -fsS \
+      'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
+
+    append_to_zshrc '# recommended by brew doctor'
+
+    # shellcheck disable=SC2016
+    append_to_zshrc 'export PATH="/usr/local/bin:$PATH"' 1
+
+    export PATH="/usr/local/bin:$PATH"
+fi
+
+# Installing services
+machine_echo "Updating Homebrew formulae ..."
+brew tap "homebrew/services"
+brew tap "caskroom/cask"
+
+# UNIX
+brew install "fish"
+brew install "git"
+brew install "gnupg2"
+brew install "openssl"
+brew install "tmux"
+brew install "vim"
+
+# PGP & Yubikey
+brew install gnupg21 pinentry-mac
+
+# Image tools
+brew install "imagemagick"
+
+# Programming languages and configurations
+brew install "node"
+brew install "npm"
+brew install "yarn"
+# NPM confortable environment
+npm install -g bower
+npm install -g gulp
+npm install -g create-react-app
+
+# Entertainment / Productivity / Work
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="/Applications" appcleaner
+brew cask install --appdir="/Applications" brave
+brew cask install --appdir="/Applications" caffeine
+brew cask install --appdir="/Applications" cyberduck
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" evernote
+brew cask install --appdir="/Applications" fontexplorer-x-pro
+brew cask install --appdir="/Applications" fontprep
+brew cask install --appdir="/Applications" firefox 
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" iterm2
+brew cask install --appdir="/Applications" keepassx
+brew cask install --appdir="/Applications" mullvad
+brew cask install --appdir="/Applications" spotify
+brew cask install --appdir="/Applications" sketch
+brew cask install --appdir="/Applications" sketch-toolbox
+brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" the-unarchiver
+brew cask install --appdir="/Applications" thunderbird
+brew cask install --appdir="/Applications" tor
+brew cask install --appdir="/Applications" torbrowser
+brew cask install --appdir="/Applications" transmission
+brew cask install --appdir="/Applications" vagrant
+brew cask install --appdir="/Applications" vagrant-manager
+brew cask install --appdir="/Applications" veracrypt
+brew cask install --appdir="/Applications" virtualbox
+brew cask install --appdir="/Applications" vlc
+
+# Security
+brew cask install --appdir="/Applications" blockblock
+brew cask install --appdir="/Applications" dhs
+brew cask install --appdir="/Applications" knockknock
+brew cask install --appdir="/Applications" oversight
+brew cask install --appdir="/Applications" security-growler
+brew cask install --appdir="/Applications" taskexplorer
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Clean up Homebrew environment
+machine_echo "Cleaning up files..."
+brew cleanup
+brew cask cleanup
+
+# Final
+machine_echo "Yo! We are all set up & ready to rock!"
+
